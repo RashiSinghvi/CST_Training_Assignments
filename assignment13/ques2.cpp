9197b0571be44fbdae6b8c9d264314ffc19142c4
RashiSinghvi <rashi.18jics046@jietjodhpur.ac.in>
2021-09-16 21:12:19 +0530

    ques 2 added

---
+++ assignment13/ques2.cpp
@@ -0,0 +1,46 @@
+//rashi singhvi
+//ques 2 Minimum increment/decrement to make array non-Increasing
+// CPP code to count the change required to
+// convert the array into non-increasing array
+#include <bits/stdc++.h>
+using namespace std;
+
+int DecreasingArray(int a[], int n)
+{
+	int sum = 0, dif = 0;
+
+	// min heap
+	priority_queue<int, vector<int>, greater<int> > pq;
+
+	// Here in the loop we will
+	// check that whether the upcoming
+	// element of array is less than top
+	// of priority queue. If yes then we
+	// calculate the difference. After
+	// that we will remove that element
+	// and push the current element in
+	// queue. And the sum is incremented
+	// by the value of difference
+	for (int i = 0; i < n; i++) {
+		if (!pq.empty() && pq.top() < a[i]) {
+			dif = a[i] - pq.top();
+			sum += dif;
+			pq.pop();
+		}
+		pq.push(a[i]);
+	}
+
+	return sum;
+}
+
+// Driver Code
+int main()
+{
+	int a[] = { 3, 1, 5, 1 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+	cout << "Output: "<<DecreasingArray(a, n);
+
+	return 0;
+}
+
